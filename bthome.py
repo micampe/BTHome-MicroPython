a632e96a6b587931aa0f04274d4723100efc0360
Dave Horton <dhorton668@outlook.com>
2025-03-26 08:49:53 -0500

    Format with Ruff

--- bthome.py
+++ bthome.py
@@ -8,11 +8,17 @@ from struct import pack
 
 # See "Advertising Payload" at https://bthome.io/format/ for details.
 _ADVERT_LENGTH_MAX = const(255)
-_ADVERT_FLAGS = bytes.fromhex('020106')  # length (02), flags indicator (01), flag bits (06)
+_ADVERT_FLAGS = bytes.fromhex(
+    "020106"
+)  # length (02), flags indicator (01), flag bits (06)
 _DEVICE_NAME_LENGTH_MAX = const(10)
 _SERVICE_DATA_UUID16 = const(0x16)
-_SERVICE_UUID16 = const(0xFCD2)   # See: https://bthome.io/images/License_Statement_-_BTHOME.pdf
-_DEVICE_INFO_FLAGS = const(0x40)  # Currently hardcoded: no encryption, regular updates, version 2
+_SERVICE_UUID16 = const(
+    0xFCD2
+)  # See: https://bthome.io/images/License_Statement_-_BTHOME.pdf
+_DEVICE_INFO_FLAGS = const(
+    0x40
+)  # Currently hardcoded: no encryption, regular updates, version 2
 
 # See "Sensor Data" table at https://bthome.io/format/ for details.
 BATTERY_UINT8 = const(0x01)
@@ -24,37 +30,43 @@ MASS_KG_UINT16 = const(0x06)
 MASS_LB_UINT16 = const(0x07)
 
 # Default value decimal places hint at precision
-battery = 0                 # percent
-temperature = 0.00          # degrees Celsius
-humidity = 0.00             # percent (relative humidity)
-pressure = 0.00             # hectoPascals (millibars)
-illuminance = 0.00          # Lux
-mass = 0.00                 # kg or lb
+battery = 0  # percent
+temperature = 0.00  # degrees Celsius
+humidity = 0.00  # percent (relative humidity)
+pressure = 0.00  # hectoPascals (millibars)
+illuminance = 0.00  # Lux
+mass = 0.00  # kg or lb
 device_name = "BTHome-MPY"  # Limit to 10 characters
+
 
 def _pack_device_name():
     assert len(device_name) > 0
     assert len(device_name) <= _DEVICE_NAME_LENGTH_MAX
-    name_type = bytes.fromhex('09')  # indicator for complete name
+    name_type = bytes.fromhex("09")  # indicator for complete name
     device_name_bytes = name_type + device_name.encode()
     device_name_bytes = bytes([len(device_name_bytes)]) + device_name_bytes
     return device_name_bytes
 
+
 # 8-bit unsigned integer with scaling of 1 (no scaling, 0 decimal places)
 def _pack_uint8_x1(object_id, value):
-    return pack('BB', object_id, value)
+    return pack("BB", object_id, value)
+
 
 # 16-bit signed integer with scalling of 100 (2 decimal places)
 def _pack_sint16_x100(object_id, value):
-    return pack('<Bh', object_id, round(value * 100))
+    return pack("<Bh", object_id, round(value * 100))
+
 
 # 16-bit unsigned integer with scalling of 100 (2 decimal places)
 def _pack_uint16_x100(object_id, value):
-    return pack('<BH', object_id, round(value * 100))
+    return pack("<BH", object_id, round(value * 100))
+
 
 # 24-bit unsigned integer with scaling of 100 (2 decimal places)
 def _pack_uint24_x100(object_id, value):
-    return pack('<BL', object_id, round(value * 100))[:-1]
+    return pack("<BL", object_id, round(value * 100))[:-1]
+
 
 # The BTHome object ID determines the number of bytes and fixed point decimal multiplier.
 def _pack_bthome_data(object_id):
@@ -77,15 +89,19 @@ def _pack_bthome_data(object_id):
     print("Packing with data:", bthome_bytes.hex().upper())
     return bthome_bytes
 
+
 # Concatenate an arbitrary number of sensor readings using parameters of sensor data constants to indicate what's included.
 def _pack_service_data(*args):
-    service_data_bytes = pack('B', _SERVICE_DATA_UUID16)  # indicates a 16-bit service UUID follows
-    service_data_bytes += pack('<h', _SERVICE_UUID16)
-    service_data_bytes += pack('B', _DEVICE_INFO_FLAGS)
+    service_data_bytes = pack(
+        "B", _SERVICE_DATA_UUID16
+    )  # indicates a 16-bit service UUID follows
+    service_data_bytes += pack("<h", _SERVICE_UUID16)
+    service_data_bytes += pack("B", _DEVICE_INFO_FLAGS)
     for object_id in args:
         service_data_bytes += _pack_bthome_data(object_id)
-    service_data_bytes = pack('B', len(service_data_bytes)) + service_data_bytes
+    service_data_bytes = pack("B", len(service_data_bytes)) + service_data_bytes
     return service_data_bytes
+
 
 # Construct advertising payload suitable for use by MicroPython's aioble.advertise(adv_data)
 def pack_advertisement(*args):
--- main.py
+++ main.py
@@ -8,16 +8,17 @@ import aioble
 import bthome
 
 BLE_ADV_INTERVAL_uS = 250000
-AWAKE_TIME_SECS = 60   # How long to spend advertising and servicing clients.
+AWAKE_TIME_SECS = 60  # How long to spend advertising and servicing clients.
 SLEEP_TIME_SECS = 120  # How long to spend in deep sleep.
 
 base_mac = unique_id()  # WiFi MAC
 bluetooth_mac = bytearray(base_mac)
-bluetooth_mac[5] += 2   # ESP32 Bluetooth MAC is always WiFi MAC + 2
+bluetooth_mac[5] += 2  # ESP32 Bluetooth MAC is always WiFi MAC + 2
 
 bthome.device_name = "DIY-sensor"
 print(bthome.device_name)
-print(hexlify(bluetooth_mac, ':').decode().upper())
+print(hexlify(bluetooth_mac, ":").decode().upper())
+
 
 async def read_sensor():
     bthome.temperature = 25  # Mocked up data for testing purposes.
@@ -26,12 +27,15 @@ async def read_sensor():
     print("Going to sleep.")
     deepsleep(SLEEP_TIME_SECS * 1000)  # Helps mitigate sensor self-heating.
 
+
 async def communicate_readings():
     print("Constructing advertising payload")
     await aioble.advertise(
         BLE_ADV_INTERVAL_uS,
-        adv_data = bthome.pack_advertisement(bthome.TEMPERATURE_SINT16, bthome.HUMIDITY_UINT16),
-        connectable = False
+        adv_data=bthome.pack_advertisement(
+            bthome.TEMPERATURE_SINT16, bthome.HUMIDITY_UINT16
+        ),
+        connectable=False,
     )
 
 
@@ -39,5 +43,6 @@ async def main():
     task1 = asyncio.create_task(read_sensor())
     task2 = asyncio.create_task(communicate_readings())
     await asyncio.gather(task1, task2)
+
 
 asyncio.run(main())
